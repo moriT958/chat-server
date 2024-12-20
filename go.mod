22e40ab035a1e747ee846811f9efd9e7687781d4
moriT958 <mkdeadg@gmail.com>
2024-12-20 14:53:26 +0900

    write message server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module simple-message-server
 
 go 1.23.3
+
+require github.com/gorilla/websocket v1.5.3
--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	http.ServeFile(w, r, "home.html")
+}
+
+func main() {
+	flag.Parse()
+
+	hub := newHub()
+	go hub.run()
+
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWS(hub, w, r)
+	})
+	log.Fatal("ListenAndServe: ", http.ListenAndServe(*addr, nil))
+}
